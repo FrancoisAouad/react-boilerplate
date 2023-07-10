6bf0ab39fce04825d00629f4418035451215394a
Francois Aouad <francois.j.aouad@gmail.com>
2023-07-10 08:28:06 +0300

    minor updates

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,6 +3,7 @@ import { Button } from 'primereact/button';
 import { Sidebar } from 'primereact/sidebar';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
+import './sidebar.css'
 
 export const MyComponent = () => {
     const [visible, setVisible] = useState(false);
@@ -10,18 +11,18 @@ export const MyComponent = () => {
     return (
         <div>
             <Button
-                label="Open Sidebar"
+                label="Sidebar"
                 onClick={() => setVisible(true)}
-                className="p-mr-2"
             />
 
-            <Sidebar visible={visible} onHide={() => setVisible(false)}>
+            <Sidebar visible={visible} onHide={() => setVisible(false)} className='fixed-sidebar'>
                 <h2>Menu</h2>
                 <ul>
                     <li>Item 1</li>
                     <li>Item 2</li>
                     <li>Item 3</li>
                 </ul>
+
             </Sidebar>
         </div>
     );
